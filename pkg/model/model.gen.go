c88326d7b5ba8268870995459b9b335502adb84d
Colin Lesnar <Colin.Lesnar@live.com>
2024-02-28 21:18:53 -0600

    Now directly using mtg.Card & mtg.Set.

--- pkg/model/model.gen.go
+++ pkg/model/model.gen.go
@@ -40,8 +40,8 @@ type Health struct {
 
 // MtgBattleCard Magic: The Gathering card data with meta data for in play. This augments the card for in play activities.
 type MtgBattleCard struct {
-	// Card Magic: The Gathering card data
-	Card *MtgCard `json:"card,omitempty"`
+	// Card The MTG card augmented for battle.
+	Card *mtg.Card `json:"card,omitempty"`
 
 	// IsAttacker boolean signaling the card is being used this turn as an attacker within play or battle.
 	IsAttacker *bool `json:"is_attacker,omitempty"`
@@ -66,129 +66,6 @@ type MtgBattleField struct {
 
 	// Lands Lands / Resources List
 	Lands []MtgBattleCard `json:"lands"`
-}
-
-// MtgCard Magic: The Gathering card data
-type MtgCard struct {
-	// Artist The artist of the card. This may not match what is on the card as MTGJSON corrects many card misprints.
-	Artist *string `json:"artist,omitempty"`
-
-	// Border If the border for this specific card is DIFFERENT than the border specified in the top level set JSON, then it will be specified here. (Example: Unglued has silver borders, except for the lands which are black bordered)
-	Border *string `json:"border,omitempty"`
-
-	// Cmc Converted mana cost. Always a number.
-	Cmc *float64 `json:"cmc,omitempty"`
-
-	// ColorIdentity The card colors by color code. ["Red", "Blue"] becomes ["R", "U"]
-	ColorIdentity *[]string `json:"colorIdentity,omitempty"`
-
-	// Colors The card colors. Usually this is derived from the casting cost, but some cards are special (like the back of dual sided cards and Ghostfire).
-	Colors *[]string `json:"colors,omitempty"`
-
-	// Flavor The flavor text of the card.
-	Flavor *string `json:"flavor,omitempty"`
-
-	// ForeignNames Foreign language names for the card, if this card in this set was printed in another language. An array of objects, each object having 'language', 'name' and 'multiverseid' keys. Not available for all sets.
-	ForeignNames *[]mtg.ForeignCardName `json:"foreignNames,omitempty"`
-
-	// Hand Maximum hand size modifier. Only exists for Vanguard cards.
-	Hand *int `json:"hand,omitempty"`
-
-	// Id A unique id for this card. It is made up by doing an SHA1 hash of setCode + cardName + cardImageName
-	Id *mtg.CardId `json:"id,omitempty"`
-
-	// ImageUrl The image url for a card. Only exists if the card has a multiverse id.
-	ImageUrl *string `json:"imageUrl,omitempty"`
-
-	// Layout The card layout. Possible values: normal, split, flip, double-faced, token, plane, scheme, phenomenon, leveler, vanguard
-	Layout *string `json:"layout,omitempty"`
-
-	// Legalities Which formats this card is legal, restricted or banned in. An array of objects, each object having 'format' and 'legality'.
-	Legalities *[]mtg.Legality `json:"legalities,omitempty"`
-
-	// Life Starting life total modifier. Only exists for Vanguard cards.
-	Life *int `json:"life,omitempty"`
-
-	// Loyalty The loyalty of the card. This is only present for planeswalkers.
-	Loyalty *string `json:"loyalty,omitempty"`
-
-	// ManaCost The mana cost of this card. Consists of one or more mana symbols. (use cmc and colors to query)
-	ManaCost *string `json:"manaCost,omitempty"`
-
-	// Multiverseid The multiverseid of the card on Wizard's Gatherer web page. Cards from sets that do not exist on Gatherer will NOT have a multiverseid. Sets not on Gatherer are: ATH, ITP, DKM, RQS, DPA and all sets with a 4 letter code that starts with a lowercase 'p'.
-	Multiverseid *mtg.MultiverseId `json:"multiverseid,omitempty"`
-
-	// Name The card name. For split, double-faced and flip cards, just the name of one side of the card. Basically each 'sub-card' has its own record.
-	Name *string `json:"name,omitempty"`
-
-	// Names Only used for split, flip and dual cards. Will contain all the names on this card, front or back.
-	Names *[]string `json:"names,omitempty"`
-
-	// Number The card number. This is printed at the bottom-center of the card in small text. This is a string, not an integer, because some cards have letters in their numbers.
-	Number *string `json:"number,omitempty"`
-
-	// OriginalText The original text on the card at the time it was printed. This field is not available for promo cards.
-	OriginalText *string `json:"originalText,omitempty"`
-
-	// OriginalType The original type on the card at the time it was printed. This field is not available for promo cards.
-	OriginalType *string `json:"originalType,omitempty"`
-
-	// Power The power of the card. This is only present for creatures. This is a string, not an integer, because some cards have powers like: "1+*"
-	Power *string `json:"power,omitempty"`
-
-	// Printings The sets that this card was printed in, expressed as an array of set codes.
-	Printings *[]mtg.SetCode `json:"printings,omitempty"`
-
-	// Rarity The rarity of the card. Examples: Common, Uncommon, Rare, Mythic Rare, Special, Basic Land
-	Rarity *string `json:"rarity,omitempty"`
-
-	// ReleaseDate The date this card was released. This is only set for promo cards. The date may not be accurate to an exact day and month, thus only a partial date may be set (YYYY-MM-DD or YYYY-MM or YYYY). Some promo cards do not have a known release date.
-	ReleaseDate *string `json:"releaseDate,omitempty"`
-
-	// Reserved Set to true if this card is reserved by Wizards Official Reprint Policy
-	Reserved *bool `json:"reserved,omitempty"`
-
-	// Rulings The rulings for the card.
-	Rulings *[]*mtg.Ruling `json:"rulings,omitempty"`
-
-	// Set The set the card belongs to (set code).
-	Set *mtg.SetCode `json:"set,omitempty"`
-
-	// SetName The set the card belongs to.
-	SetName *string `json:"setName,omitempty"`
-
-	// Source For promo cards, this is where this card was originally obtained. For box sets that are theme decks, this is which theme deck the card is from.
-	Source *string `json:"source,omitempty"`
-
-	// Starter Set to true if this card was only released as part of a core box set. These are technically part of the core sets and are tournament legal despite not being available in boosters.
-	Starter *bool `json:"starter,omitempty"`
-
-	// Subtypes The subtypes of the card. These appear to the right of the dash in a card type. Usually each word is its own subtype. Example values: Trap, Arcane, Equipment, Aura, Human, Rat, Squirrel, etc.
-	Subtypes *[]string `json:"subtypes,omitempty"`
-
-	// Supertypes The supertypes of the card. These appear to the far left of the card type. Example values: Basic, Legendary, Snow, World, Ongoing
-	Supertypes *[]string `json:"supertypes,omitempty"`
-
-	// Text The oracle text of the card. May contain mana symbols and other symbols.
-	Text *string `json:"text,omitempty"`
-
-	// Timeshifted If this card was a timeshifted card in the set.
-	Timeshifted *bool `json:"timeshifted,omitempty"`
-
-	// Toughness The toughness of the card. This is only present for creatures. This is a string, not an integer, because some cards have toughness like: "1+*"
-	Toughness *string `json:"toughness,omitempty"`
-
-	// Type The card type. This is the type you would see on the card if printed today. Note: The dash is a UTF8 'long dash' as per the MTG rules
-	Type *string `json:"type,omitempty"`
-
-	// Types The types of the card. These appear to the left of the dash in a card type. Example values: Instant, Sorcery, Artifact, Creature, Enchantment, Land, Planeswalker
-	Types *[]string `json:"types,omitempty"`
-
-	// Variations If a card has alternate art (for example, 4 different Forests, or the 2 Brothers Yamazaki) then each other variation's multiverseid will be listed here, NOT including the current card's multiverseid.
-	Variations *[]string `json:"variations,omitempty"`
-
-	// Watermark The watermark on the card. Note: Split cards don't currently have this field set, despite having a watermark on each side of the split card.
-	Watermark *string `json:"watermark,omitempty"`
 }
 
 // MtgGameManager Magic: The Gathering Game Manager and Game meta data
@@ -218,19 +95,13 @@ type MtgGameManagerGamePhase string
 // MtgPlayer Magic: The Gathering Standard game play meta data
 type MtgPlayer struct {
 	// Deck Deck cards List. Rules of the Deck: 60 Card minimum, 4 limit on copies (exception for basic lands).
-	Deck []MtgCard `json:"deck"`
+	Deck []mtg.Card `json:"deck"`
 
 	// Name Player Name
 	Name string `json:"name"`
 
 	// Zones Cards in each Zone
 	Zones []MtgZones `json:"zones"`
-}
-
-// MtgSet Magic: The Gathering card data
-type MtgSet struct {
-	// Name Set Name
-	Name *string `json:"name,omitempty"`
 }
 
 // MtgZones Magic: The Gathering Standard game play meta data
